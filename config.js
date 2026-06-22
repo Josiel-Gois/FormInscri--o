c44eb27eb8cc1bcc3ee4afa374998f3ad440a9f2
Josiel-Gois <josieltnt@gmail.com>
2026-06-22 13:04:47 -0300

    ajusta mensageria

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // CONFIGURAÇÃO LOCAL - NÃO COMITE NO GITHUB
 // Configure as URLs reais do seu Google Apps Script aqui
 
-const API_URL = 'https://script.google.com/macros/s/AKfycby_GfeRHf15eBGal7xtxKn_re6F6q2Q6oyplPxgHXjKn5WcF6_vRBmlk_imadGCDjeLNw/exec';
-const PUBLIC_API_URL = 'https://script.google.com/macros/s/AKfycby_GfeRHf15eBGal7xtxKn_re6F6q2Q6oyplPxgHXjKn5WcF6_vRBmlk_imadGCDjeLNw/exec';
+const API_URL = 'https://script.google.com/macros/s/AKfycbxGGrLUyVeYqp92JflZh1TLeZwfyl7G5sCyZU2ZZK2iO2qNRsVBY0SxyvE3VniAUd8ZVw/exec';
+const PUBLIC_API_URL = 'https://script.google.com/macros/s/AKfycbxGGrLUyVeYqp92JflZh1TLeZwfyl7G5sCyZU2ZZK2iO2qNRsVBY0SxyvE3VniAUd8ZVw/exec';
 
 
