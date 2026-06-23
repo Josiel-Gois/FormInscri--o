863f314f11b74ac7c6921f3ae6e3c02f21280418
Josiel-Gois <josieltnt@gmail.com>
2026-06-22 22:50:17 -0300

    fix: atualizar URL da API em config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 // CONFIGURAÇÃO LOCAL - NÃO COMITE NO GITHUB
 // Configure as URLs reais do seu Google Apps Script aqui
 
-const API_URL = 'https://script.google.com/macros/s/AKfycbwMVwmdL_NxUqMEsu8jywbQ7Lf0aCOvO0AApzzcOJhg5g5y534u9ccssTPUVazQw-vwCA/exec';
-const PUBLIC_API_URL = 'https://script.google.com/macros/s/AKfycbwMVwmdL_NxUqMEsu8jywbQ7Lf0aCOvO0AApzzcOJhg5g5y534u9ccssTPUVazQw-vwCA/exec';
+const API_URL = 'https://script.google.com/macros/s/AKfycbwRByW9RehX5ue9fAjkKbPA66QupPPzUxk_NdMmxP5FZLuvVDoh7i38SGK_sNs7T98J7g/exec';
+const PUBLIC_API_URL = 'https://script.google.com/macros/s/AKfycbwRByW9RehX5ue9fAjkKbPA66QupPPzUxk_NdMmxP5FZLuvVDoh7i38SGK_sNs7T98J7g/exec';
 const CONFIG = {
-    API_URL: 'https://script.google.com/macros/s/AKfycbwMVwmdL_NxUqMEsu8jywbQ7Lf0aCOvO0AApzzcOJhg5g5y534u9ccssTPUVazQw-vwCA/exec'
+    API_URL: 'https://script.google.com/macros/s/AKfycbwRByW9RehX5ue9fAjkKbPA66QupPPzUxk_NdMmxP5FZLuvVDoh7i38SGK_sNs7T98J7g/exec'
 };
